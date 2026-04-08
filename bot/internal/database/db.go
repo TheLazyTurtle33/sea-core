4fdd853d3f125baf83fdbcc9001ec3c9eae530bc
lazyturtle33 <lazyturtle3333@gmail.com>
2026-04-08 22:59:41 +0200

    database works

--- bot/internal/database/db.go
+++ bot/internal/database/db.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/TheLazyTurtle33/sea-core/bot/internal/cleanup"
+	"github.com/TheLazyTurtle33/sea-core/bot/internal/logger"
 	_ "github.com/lib/pq"
 )
 
 const (
-	host   = "localhost"
+	host   = "postgres"
 	port   = 5432
 	user   = "turt"
 	dbname = "sea_db"
@@ -17,10 +19,59 @@ const (
 
 var password = os.Getenv("POSTGRES_PASSWORD")
 
+type Database struct {
+	db *sql.DB
+}
+
+var instance *Database
+
+func Get() *Database {
+	if instance != nil {
+		return instance
+	}
+
+	psqlInfo := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
+		user, password, host, port, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		logger.Error("failed to connect to database", err)
+		return nil
+	}
+
+	instance = &Database{db: db}
+	cleanup.RegisterCleaner(&cleaner{})
+	return instance
+
+}
+
+func (d *Database) Query(query string, args ...any) (*sql.Rows, error) {
+	return d.db.Query(query, args...)
+}
+
+func (d *Database) Exec(query string, args ...any) (sql.Result, error) {
+	return d.db.Exec(query, args...)
+}
+
+type cleaner struct {
+	cleanup.Cleaner
+}
+
+func (c *cleaner) Clean() {
+	if instance == nil {
+		return
+	}
+	err := instance.db.Close()
+	if err != nil {
+		logger.Error("failed to close database connection", err)
+	}
+}
+
 func Test() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	psqlInfo := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
+		user, password, host, port, dbname)
+	logger.Log("connecting to database with info", "info", psqlInfo)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
