3e1afe48fa990cc35a2fe66aaf4f23c89cd1ea08
lazyturtle33 <lazyturtle3333@gmail.com>
2026-04-18 23:34:49 +0200

    added raid message

--- bot/internal/twitch/eventsub/raid.go
+++ bot/internal/twitch/eventsub/raid.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/TheLazyTurtle33/sea-core/bot/internal/context"
 	"github.com/TheLazyTurtle33/sea-core/bot/internal/logger"
+	twitchapi "github.com/TheLazyTurtle33/sea-core/bot/internal/twitch/api"
 	"github.com/TheLazyTurtle33/sea-core/shared/obs"
 )
 
@@ -27,48 +28,62 @@ const (
 
 func HandleRaid(data raidData) {
 	if context.Get().GetBroadcaster().Id == data.FromBroadcasterUserID {
+		if _, err := twitchapi.AsBot().SendMessage(
+			fmt.Sprintf("BYYEEE EVERYONE :3 have fun in %s's channel ^w^, remember to behave >:c, raid messages coming in!",
+				data.ToBroadcasterUserName,
+			),
+		); err != nil {
+			logger.Error("Raid: Error sending message", err)
+		}
+		if _, err := twitchapi.AsBot().SendMessage("TURTLE RAID! TURTLE RAID! TURTLE RAID! TURTLE RAID! TURTLE RAID! TURTLE RAID! "); err != nil {
+			logger.Error("Raid: Error sending message", err)
+		}
+		// sub raid, still need emots lol
+		// if _, err := twitchapi.AsBot().SendMessage(); err != nil {
+		// 	logger.Error("Raid: Error sending message", err)
+		// }
 	} else {
 		client, err := obs.Get()
 		if err != nil {
-			logger.Error("Error getting OBS client", err)
+			logger.Error("Raid: Error getting OBS client", err)
 			return
 		}
 
 		raidText := fmt.Sprintf("%s Washed in \n with %d castaways", data.FromBroadcasterUserName, data.Viewers)
 
 		if _, err = client.SetSourceVisability("RaidText", false); err != nil {
-			logger.Error("Error hiding RaidText", err)
+			logger.Error("Raid: Error hiding RaidText", err)
 			return
 		}
 
 		if _, err = client.SetSourceVisability("Raid", true); err != nil {
-			logger.Error("Error showing Raid group", err)
+			logger.Error("Raid: Error showing Raid group", err)
 			return
 		}
 
 		if _, err = client.SetTextSourceText("RaidText", raidText); err != nil {
-			logger.Error("Error uppdating RaidText", err)
+			logger.Error("Raid: Error uppdating RaidText", err)
 			return
 		}
 
 		time.Sleep(TextShowWait)
 
 		if _, err = client.SetSourceVisability("RaidText", true); err != nil {
-			logger.Error("Error showing RaidText", err)
+			logger.Error("Raid: Error showing RaidText", err)
 			return
 		}
 
 		time.Sleep(GroupHideWait)
 
 		if _, err = client.SetSourceVisability("Raid", false); err != nil {
-			logger.Error("Error showing Raid group", err)
+			logger.Error("Raid: Error showing Raid group", err)
 			return
 		}
 
 		time.Sleep(TextHideWait)
 
 		if _, err = client.SetSourceVisability("RaidText", false); err != nil {
-			logger.Error("Error hiding RaidText", err)
+			logger.Error("Raid: Error hiding RaidText", err)
 			return
 		}
 
